72edf01b7321a730bb5a665c70e96479cf2d9741
Andrew Hay Kurtz <andrew.hay.kurtz@gmail.com>
2016-03-24 17:07:23 -0700

    rename project to pocket-home

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -30,7 +30,7 @@
 #if ! JUCE_DONT_DECLARE_PROJECTINFO
 namespace ProjectInfo
 {
-    const char* const  projectName    = "PokeLaunch";
+    const char* const  projectName    = "pocket-home";
     const char* const  versionString  = "0.0.1";
     const int          versionNumber  = 0x1;
 }
