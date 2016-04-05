a4ecbabb85082551a4248ff97a8f79dc10ae11c1
Jason Abele <jason@nextthing.co>
2016-04-04 17:44:07 -0700

    wifitest: add code and make target for wifitest

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ all:
 
 clean:
 	cd Builds/LinuxMakefile && CONFIG=Release make clean
+
+wifitest:
+	cd Builds/LinuxMakefile && CONFIG=Release make -f UnitTests.mk ../../build/Release/wifitest
---
+++ Source/WifiTest.cpp
@@ -0,0 +1,96 @@
+#include "WifiStatus.h"
+
+File absoluteFileFromPath(const String &path) {
+  return File::isAbsolutePath(path) ? File(path)
+                                    : File::getCurrentWorkingDirectory().getChildFile(path);
+}
+
+// TODO: allow user overrides of asset files
+File assetFile(const String &fileName) {
+  auto devFile = absoluteFileFromPath("../../assets/" + fileName);
+  File assetFile;
+
+  assetFile = devFile;
+  
+  return assetFile;
+}
+
+class TestWifiStatus {
+public:
+  WifiStatus wifiStatus;
+
+  WifiStatus &getWifiStatus();
+
+  void test_populate();
+  void test_ap_list();
+  void test_enabled();
+  void test_connected();
+  WifiAccessPoint * test_find_ap(String *ssid);
+  void test_set_connected(WifiAccessPoint *ap);
+  void test_set_disconnected();
+};
+
+void TestWifiStatus::test_populate() {
+  // Populate with dummy data
+  std::cout << "Populating wifiStatus from wifi.json ..." << std::endl;
+  auto ssidListFile = assetFile("wifi.json");
+  wifiStatus.populateFromJson(JSON::parse(ssidListFile));
+}
+
+void TestWifiStatus::test_ap_list() {
+  std::cout << "Printing known AP's from wifiStatus ..." << std::endl;
+  for (auto ap : wifiStatus.accessPoints) {
+	  std::cout << ap->ssid << std::endl;
+  }
+}
+
+void TestWifiStatus::test_enabled() {
+  std::cout << "Is wifiStatus enabled?" << std::endl;
+  std::cout << wifiStatus.enabled << std::endl;
+}
+
+void TestWifiStatus::test_connected() {
+  std::cout << "Is wifiStatus connected?" << std::endl;
+  std::cout << wifiStatus.connected << std::endl;
+}
+
+WifiAccessPoint * TestWifiStatus::test_find_ap(String *ssid) {
+  std::cout << "Finding " << *ssid << " within wifiStatus..." << std::endl;
+  for (auto ap : wifiStatus.accessPoints) {
+    if (ap->ssid == *ssid) {
+      return ap;
+    }
+  }
+  std::cout << "Failed searching for " << *ssid << std::endl;
+  return NULL;
+}
+
+void TestWifiStatus::test_set_connected(WifiAccessPoint *ap) {
+  std::cout << "Connecting wifiStatus to " << ap->ssid << std::endl;
+  wifiStatus.setConnectedAccessPoint(ap);
+}
+
+void TestWifiStatus::test_set_disconnected() {
+  std::cout << "Disconnecting wifiStatus ..." << std::endl;
+  wifiStatus.setDisconnected();
+}
+
+int main() {
+  TestWifiStatus wifi;
+  WifiAccessPoint *ap;
+  String ssid = "NTC";
+
+  wifi.test_populate();
+  wifi.test_ap_list();
+  wifi.test_enabled();
+  wifi.test_connected();
+
+  ap = wifi.test_find_ap(&ssid);
+  wifi.test_set_connected(ap);
+  wifi.test_enabled();
+  wifi.test_connected();
+
+  wifi.test_set_disconnected();
+  wifi.test_enabled();
+  wifi.test_connected();
+}
