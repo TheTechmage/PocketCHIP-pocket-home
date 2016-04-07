1c7ee50452c95bb38730108d4c28839c19bc6c94
Jason Abele <jason@nextthing.co>
2016-04-06 18:04:26 -0700

    wifistatus: add hacky parser for nmcli ssid list

--- Source/WifiStatus.cpp
+++ Source/WifiStatus.cpp
@@ -1,3 +1,5 @@
+#include <map>
+
 #include "WifiStatus.h"
 #include "../JuceLibraryCode/JuceHeader.h"
 
@@ -68,16 +70,53 @@ void WifiStatus::setDisconnected() {
 void WifiStatus::populateFromJson(const var &json) {
   connectedAccessPoint = nullptr;
   connected = false;
+  String ssidList;
+  std::map<String, String> tag_map;
+  ChildProcess nmproc;
 
   accessPoints.clear();
 
-  for (const auto &apJson : *json.getArray()) {
-    auto ap = new WifiAccessPoint();
-    ap->ssid = apJson["name"];
-    ap->signalStrength = apJson["strength"];
-    ap->requiresAuth = apJson["auth"];
-    accessPoints.add(ap);
+  auto cmd = "nmcli -m multiline -f SSID,SECURITY,SIGNAL d wifi list ifname wlan0";
+  DBG("wifi cmd: " << cmd);
+  nmproc.start(cmd);
+  ssidList = nmproc.readAllProcessOutput();
+
+  auto addAccessPoint = [](std::map<String, String> &keyvals, OwnedArray<WifiAccessPoint> &aps) {
+    aps.add( new WifiAccessPoint {
+      keyvals["SSID"] == "--" ? "HiddenSSID" : keyvals["SSID"],
+      keyvals["SIGNAL"].getIntValue(),
+      keyvals["SECURITY"].isNotEmpty(), //FIXME: Assumes all security types equal
+    });
+  };
+
+  auto split = [](const String &orig, const String delim) {
+    std::vector<String> elems;
+    int index = 0;
+    auto remainder = orig.substring(index);
+    while (remainder.length()) {
+      index = remainder.indexOf(delim);
+      if (index < 0) {
+        elems.push_back(remainder);
+	break;
+      }
+      elems.push_back(remainder.substring(0,index));
+      remainder = remainder.substring(index+1);
+    }
+    return elems;
+  };
+
+  for (const String& tag : split(ssidList, "\n")) {
+    auto key_val = split(tag, ":");
+    DBG("Produced {" << key_val[0] << ": " << key_val[1].trimStart() << "}");
+    if (key_val[0] == "SSID" && !tag_map.empty()) {
+      DBG("Adding non-empty tagmap to accessPoints");
+      addAccessPoint(tag_map, accessPoints);
+      tag_map.clear();
+    }
+    tag_map.insert(std::make_pair(key_val[0], key_val[1].trimStart()));
   }
+  addAccessPoint(tag_map, accessPoints);
+  tag_map.clear();
 }
 
 WifiStatus::Listener::Listener() {}
