933fd2a30162adc0fb9d4e3e0b796b18d46b5e3f
Jason Abele <jason@nextthing.co>
2016-04-15 09:42:30 -0700

    wifitest: refactor WifiTest.cpp to run tests on Timer ticks
    This is the start of a test that will allow for async callbacks in the
    wifi layer.

--- Source/WifiTest.cpp
+++ Source/WifiTest.cpp
@@ -1,3 +1,4 @@
+#include "../JuceLibraryCode/JuceHeader.h"
 #include "WifiStatus.h"
 
 File absoluteFileFromPath(const String &path) {
@@ -11,119 +12,187 @@ File assetFile(const String &fileName) {
   File assetFile;
 
   assetFile = devFile;
-  
+
   return assetFile;
 }
 
-class TestWifiStatus {
+
+class WifiTestApplication;
+
+class WifiNextStepTimer : public Timer {
 public:
+  WifiNextStepTimer();
+  ~WifiNextStepTimer();
+
+  void timerCallback() override;
+
+  WifiTestApplication *wifiTestApp;
+};
+
+WifiNextStepTimer::WifiNextStepTimer() : wifiTestApp() {}
+WifiNextStepTimer::~WifiNextStepTimer() {}
+
+class WifiTestApplication : public JUCEApplication {
+public:
+  const String getApplicationName() override;
+  const String getApplicationVersion() override;
+  void shutdown() override;
+
   WifiStatusNM wifiStatusNM;
   WifiStatusJson wifiStatusJson;
   WifiStatus *wifiStatus;
 
   WifiStatus &getWifiStatus();
 
-  void test_populate();
-  void test_ap_list();
-  void test_enabled();
-  void test_connected();
-  WifiAccessPoint * test_find_ap(String *ssid);
-  void test_set_disabled();
-  void test_set_enabled();
-  void test_set_connected(WifiAccessPoint *ap, const String psk);
-  void test_set_disconnected();
+  int currentStep = 0;
+  void nextTestStep();
+
+  String ssid = "BOGUS_SSID";
+  String psk = "";
+  void initialise(const String &commandLine);
+
+private:
+  Array< std::function<void(WifiStatus *wifiStatus)> > testSteps;
+  WifiNextStepTimer testStepTimer;
 };
 
-WifiStatus &TestWifiStatus::getWifiStatus() {
+void WifiNextStepTimer::timerCallback() {
+  DBG(__func__);
+  if (wifiTestApp) {
+    wifiTestApp->nextTestStep();
+  }
+}
+
+void WifiTestApplication::nextTestStep() {
+  std::cout << "Executing testStep" << currentStep << std::endl;
+  if (currentStep < testSteps.size()) {
+    testSteps[currentStep](wifiStatus);
+    currentStep++;
+    return;
+  }
+
+  testStepTimer.stopTimer();
+  quit();
+}
+
+
+void WifiTestApplication::initialise(const String &commandLine) {
+  StringArray args;
+  args.addTokens(commandLine, true);
+
+  if (args.contains("--help")) {
+    std::cerr << "arguments:" << std::endl;
+    std::cerr << "  --help:      Print usage help" << std::endl;
+    std::cerr << "  --fakewifi:  Use fake WifiStatus" << std::endl;
+    std::cerr << "  --ssid <SSID>" << std::endl;
+    std::cerr << "  --psk <PSK>" << std::endl;
+    quit();
+  }
+
+  if (args.contains("--fakewifi"))
+    wifiStatus = &wifiStatusJson;
+  else
+    wifiStatus = &wifiStatusNM;
+
+  if (args.contains("--ssid"))
+    ssid = args[args.indexOf("--ssid")+1];
+  if (args.contains("--psk"))
+    psk = args[args.indexOf("--psk")+1];
+
+  std::cout << "Using SSID = " << ssid << std::endl;
+  std::cout << "Using PSK = " << psk << std::endl;
+  
+  auto test_set_enabled = [](WifiStatus *wifiStatus) {
+    std::cout << "Enabling wifiStatus ..." << std::endl;
+    wifiStatus->setEnabled();
+  };
+
+  auto test_set_disabled = [](WifiStatus *wifiStatus) {
+    std::cout << "Disabling wifiStatus ..." << std::endl;
+    wifiStatus->setDisabled();
+  };
+
+  auto test_initialize = [](WifiStatus *wifiStatus) {
+    std::cout << "Initializing wifiStatus ..." << std::endl;
+    wifiStatus->initializeStatus();
+  };
+
+  auto test_ap_list = [](WifiStatus *wifiStatus) {
+    std::cout << "Printing known AP's from wifiStatus ..." << std::endl;
+    for (auto ap : *wifiStatus->nearbyAccessPoints()) {
+  	  std::cout << ap->ssid << std::endl;
+    }
+  };
+
+  auto test_enabled = [](WifiStatus *wifiStatus) {
+    std::cout << "Is wifiStatus enabled?" << std::endl;
+    std::cout << wifiStatus->isEnabled() << std::endl;
+  };
+
+  auto test_connected = [](WifiStatus *wifiStatus) {
+    std::cout << "Is wifiStatus connected?" << std::endl;
+    std::cout << wifiStatus->isConnected() << std::endl;
+  };
+
+  auto test_find_and_connect = [this](WifiStatus *wifiStatus) {
+    std::cout << "Finding SSID " << ssid << " within wifiStatus..." << std::endl;
+    for (auto ap : *wifiStatus->nearbyAccessPoints()) {
+      if (ap->ssid == ssid) {
+	std::cout << "Found: " << ap->ssid << std::endl;
+        std::cout << "Connecting wifiStatus to " << ap->ssid << std::endl;
+        wifiStatus->setConnectedAccessPoint(ap, psk);
+        return;
+      }
+    }
+    std::cout << "Failed searching for " << ssid << std::endl;
+  };
+
+  auto test_connected_ap = [this](WifiStatus *wifiStatus) {
+    std::cout << "Connected AP SSID = " <<
+	    wifiStatus->connectedAccessPoint()->ssid << std::endl;
+  };
+
+  auto test_explicit_disconnect = [this](WifiStatus *wifiStatus) {
+    std::cout << "Disconnecting wifiStatus..." << std::endl;
+    wifiStatus->setDisconnected();
+  };
+
+  testSteps.add(test_initialize);
+  testSteps.add(test_enabled);
+  testSteps.add(test_connected);
+  testSteps.add(test_set_enabled);
+  testSteps.add(test_enabled);
+  testSteps.add(test_connected);
+  testSteps.add(test_ap_list);
+  testSteps.add(test_find_and_connect);
+  testSteps.add(test_enabled);
+  testSteps.add(test_connected);
+  testSteps.add(test_connected_ap);
+  testSteps.add(test_explicit_disconnect);
+  testSteps.add(test_enabled);
+  testSteps.add(test_connected);
+  testSteps.add(test_set_disabled);
+  testSteps.add(test_enabled);
+  testSteps.add(test_connected);
+
+  testStepTimer.wifiTestApp = this;
+  testStepTimer.startTimer(1000);
+}
+
+WifiStatus &WifiTestApplication::getWifiStatus() {
   return *this->wifiStatus;
 }
 
-void TestWifiStatus::test_populate() {
-  // Populate with dummy data
-  std::cout << "Initializing wifiStatus ..." << std::endl;
-  getWifiStatus().initializeStatus();
+void WifiTestApplication::shutdown() {
 }
 
-void TestWifiStatus::test_ap_list() {
-  std::cout << "Printing known AP's from wifiStatus ..." << std::endl;
-  for (auto ap : *getWifiStatus().nearbyAccessPoints()) {
-	  std::cout << ap->ssid << std::endl;
-  }
+
+const String WifiTestApplication::getApplicationName() {
+  return "wifitest Application";
 }
 
-void TestWifiStatus::test_enabled() {
-  std::cout << "Is wifiStatus enabled?" << std::endl;
-  std::cout << getWifiStatus().isEnabled() << std::endl;
+const String WifiTestApplication::getApplicationVersion() {
+  return "1.0";
 }
 
-void TestWifiStatus::test_connected() {
-  std::cout << "Is wifiStatus connected?" << std::endl;
-  std::cout << getWifiStatus().isConnected() << std::endl;
-}
-
-WifiAccessPoint * TestWifiStatus::test_find_ap(String *ssid) {
-  std::cout << "Finding " << *ssid << " within wifiStatus..." << std::endl;
-  for (auto ap : *getWifiStatus().nearbyAccessPoints()) {
-    if (ap->ssid == *ssid) {
-      return ap;
-    }
-  }
-  std::cout << "Failed searching for " << *ssid << std::endl;
-  return NULL;
-}
-
-void TestWifiStatus::test_set_enabled() {
-  std::cout << "Enabling wifiStatus ..." << std::endl;
-  getWifiStatus().setEnabled();
-}
-
-void TestWifiStatus::test_set_disabled() {
-  std::cout << "Disabling wifiStatus ..." << std::endl;
-  getWifiStatus().setDisabled();
-}
-
-void TestWifiStatus::test_set_connected(WifiAccessPoint *ap, const String psk) {
-  std::cout << "Connecting wifiStatus to " << ap->ssid << std::endl;
-  getWifiStatus().setConnectedAccessPoint(ap, psk);
-}
-
-void TestWifiStatus::test_set_disconnected() {
-  std::cout << "Disconnecting wifiStatus ..." << std::endl;
-  getWifiStatus().setDisconnected();
-}
-
-int main() {
-  TestWifiStatus wifi;
-  WifiAccessPoint *ap;
-  String ssid = "BOGUS_SSID";
-  String psk = "BOGUS_PSK";
-
-  wifi.wifiStatus = &wifi.wifiStatusNM;
-
-  wifi.test_set_enabled();
-  wifi.test_enabled();
-  wifi.test_connected();
-
-  wifi.test_set_disabled();
-  wifi.test_enabled();
-  wifi.test_connected();
-
-  wifi.test_set_enabled();
-  wifi.test_enabled();
-  wifi.test_connected();
-
-  wifi.test_populate();
-  wifi.test_ap_list();
-  wifi.test_enabled();
-  wifi.test_connected();
-
-  ap = wifi.test_find_ap(&ssid);
-  wifi.test_set_connected(ap, psk);
-  wifi.test_enabled();
-  wifi.test_connected();
-
-  //wifi.test_set_disconnected();
-  wifi.test_enabled();
-  wifi.test_connected();
-
-}
+START_JUCE_APPLICATION(WifiTestApplication)
